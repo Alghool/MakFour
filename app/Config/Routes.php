f382de831ee49abec3923450f4bf46ba3c4f97c8
Mahmoud Alghool <Mahmoud.alghool@digipalms.com>
2024-03-29 03:04:17 +0200

    add users model

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,7 +6,8 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 
-$routes->get('/', 'Home::index');
+$routes->get('/', 'Home::index',  ['filter' => \App\Filters\AuthFilter::class]);
 
-$routes->get('login', 'LoginController::login');
+$routes->get('/login', 'LoginController::login');
+
 
---
+++ app/Models/UserModel.php
@@ -0,0 +1,31 @@
+<?php
+
+
+use CodeIgniter\Model;
+
+class UserModel extends Model
+{
+	protected $table      = 'users';
+	protected $primaryKey = 'user_id';
+
+	protected $useAutoIncrement = true;
+
+	protected $returnType     = 'array';
+	protected $useSoftDeletes = false;
+
+	protected $validationRules = [
+		'user_id'      => 'if_exist|numeric',
+		'email'        => 'max_length[254]|valid_email|is_unique[users.email,users.user_id,{user_id}]',
+	];
+
+	protected $validationMessages = [
+		'email' => [
+			'is_unique' => 'Sorry. That email has already been taken. Please choose another.',
+		],
+	];
+
+	protected $allowedFields = ['name', 'email', 'password', 'type', 'pic', 'phone'];
+
+
+
+}
